334164d9cd0df9b14e225632f1faca0f186f7a32
madahatter <adamharper9@gmail.com>
2018-05-16 10:25:37 -0400

    register and eval qs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './Login'
+import Register from './Register'
+import EvaluationQuestions from './EvaluationQuestions.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Login></Login>
+      <Register></Register>
+      <EvaluationQuestions></EvaluationQuestions>
       </div>
     );
   }
