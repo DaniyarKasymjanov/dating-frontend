7ac9d8f36b6fc1c44d9ebb059a687ec3d1e27f88
Dickson Lee <dicksont_lee@hotmail.com>
2018-05-16 15:05:34 -0400

    gg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Link } from 'react-router-dom';
-import NavBar from './NavBar'
-import Login from './Login'
-import Register from './Register'
+import NavBar from './NavBar.js'
+import Login from './Login.js'
+import Register from './Register.js'
 import EvaluationQuestions from './EvaluationQuestions.js'
-import Home from './Home'
-import Search from './Search'
-import Profile from './Profile'
-import Favorites from './Favorites'
+import Home from './Home.js'
+import Search from './Search.js'
+import Profile from './Profile.js'
+import Favorites from './Favorites.js'
 import './App.css';
 
 class App extends Component {
 
   renderHome = () => {
     return(<Home/>)
-  }
-  
-  renderProfile = () => {
-    return(<Profile/>)
   }
 
   renderSearch = () => {
@@ -39,7 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Route exact path={/^\/(?!(login|register)).*$/} render={this.renderNavbar}/>
+      <Route exact path="/navbar" render={this.renderNavBar}/>
       <Route exact path="/login" render={this.renderLogin}/>
       <Route exact path="/" render={this.renderHome}/>
       <Route exact path="/search" render={this.renderSearch}/>
---
+++ src/Spotlight.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Spotlight extends React.Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>
+        <div>Name</div>
+        <div>img</div>
+        <div>favorites</div>
+      </div>
+    )
+  }
+}
+
+export default Spotlight
