66172b9f74de8239e68722abb7cf06ed48485aca
Dickson Lee <dicksont_lee@hotmail.com>
2018-05-17 14:02:39 -0400

    spot

--- src/Spotlight.js
+++ src/Spotlight.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import { SpotlightGrid } from './Styled';
 
 class Spotlight extends Component{
   constructor(){
@@ -9,14 +10,14 @@ class Spotlight extends Component{
   }
   render(){
     return(
-      <div>
+      <SpotlightGrid>
           <h1>Spotlight</h1>
           <h3>img</h3>
           <Link to="/profile">{this.props.username}</Link>
           <div>
             <Link to="/favorites"> Favorites</Link>
           </div>
-      </div>
+      </SpotlightGrid>
     )
   }
 }
