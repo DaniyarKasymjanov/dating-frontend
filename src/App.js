fd74892b500c7c4cd6adf34fe3cb754f6bcbcb5c
Daniyar Kasymjanov <kasymda@gmail.com>
2018-05-19 14:10:24 -0400

    dan

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
+const socket = io();
+
 class App extends Component {
+componentDidMount(){
+  socket.on('receive_msg', (msg) => {
+    console.log(msg)
+  })
+}
+  handleClick = ()=> {
+    socket.emit('send_msg', {msg: 'Hey man!'})
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +21,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.handleClick}>test</button>
       </div>
     );
   }
